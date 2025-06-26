204fb7983d88849a14aca488dd6ffeecb6f0ee56
llj <liulinjie@zhihu.com>
2025-06-27 00:46:40 +0800

    home 结构

---
+++ component/src/main/java/com/comeon/android/app/home/main/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.comeon.android.app.home.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.comeon.android.component.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        const val TAG = "MainFragment"
+    }
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMainBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
