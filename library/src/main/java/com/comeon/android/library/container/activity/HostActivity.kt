c093bab16446097fa60d63bbe3a9c978e24a5604
llj <liulinjie@zhihu.com>
2025-06-30 23:18:47 +0800

    HostActivity

--- library/src/main/java/com/comeon/android/library/container/activity/HostActivity.kt
+++ library/src/main/java/com/comeon/android/library/container/activity/HostActivity.kt
@@ -1,3 +1,32 @@
 package com.comeon.android.library.container.activity
 
-open class HostActivity : CoActivity()
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.comeon.android.library.databinding.ActivityHostContainerBinding
+
+open class HostActivity : CoActivity() {
+
+    protected lateinit var binding: ActivityHostContainerBinding
+
+    private val hostFragment: Fragment by lazy {
+        buildHostFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHostContainerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.hostFragmentContainer.id, hostFragment, getFragmentTag())
+            .commitAllowingStateLoss()
+    }
+
+    protected open fun buildHostFragment(): Fragment {
+        return Fragment()
+    }
+
+    protected open fun getFragmentTag() = "HostFragment"
+
+}
