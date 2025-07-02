25bcfdf3ae660b35d2efbf7d7e61c2ecbf792a56
llj <liulinjie@zhihu.com>
2025-07-03 00:46:57 +0800

    controller view - temp

--- component/src/main/java/com/comeon/android/app/home/video/ui/activity/PlayerActivity.kt
+++ component/src/main/java/com/comeon/android/app/home/video/ui/activity/PlayerActivity.kt
@@ -1,5 +1,7 @@
 package com.comeon.android.app.home.video.ui.activity
 
+import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.Fragment
 import com.comeon.android.app.home.video.ui.fragment.PlayerFragment
 import com.comeon.android.library.container.activity.HostActivity
@@ -8,6 +10,11 @@ class PlayerActivity : HostActivity() {
 
     companion object {
         const val TAG = "PlayerActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
     }
 
     override fun buildHostFragment(): Fragment {
