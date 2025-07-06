adc8f55f2261ca7426fd834eb7f3a7d0fe22ae95
liulinjie <liulinjie@zhihu.com>
2025-07-06 22:31:04 +0800

    ImageCheckView

---
+++ library/src/main/java/com/comeon/android/library/ui/widget/ImageCheckView.kt
@@ -0,0 +1,77 @@
+package com.comeon.android.library.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+/**
+ * 选中状态切换imageView
+ * 可以按照CheckBox的方式使用，支持选中状态切换image。
+ *
+ * example:
+ *
+ * 1.
+ * drawable/check_bg.xml
+ * <selector xmlns:android="http://schemas.android.com/apk/res/android">
+ *     <item android:state_checked="false" android:drawable="@drawable/check"/>
+ *     <item android:state_checked="true" android:drawable="@drawable/uncheck"/>
+ * </selector>
+ *
+ * 2.
+ * <com.comeon.android.library.ui.widget.ImageCheckView>
+ *  android:src="@drawable/check_bg"
+ * </com.comeon.android.library.ui.widget.ImageCheckView>
+ *
+ */
+class ImageCheckView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    private var isChecked = false
+
+    private val CHECKED_STATE_SET = intArrayOf(
+        android.R.attr.state_checked
+    )
+
+    private var onCheckListener: OnCheckListener? = null
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray? {
+        val drawableState = super.onCreateDrawableState(extraSpace + 1)
+        if (isChecked) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET)
+        }
+        return drawableState
+    }
+
+    fun setChecked(checked: Boolean) {
+        if (isChecked != checked) {
+            isChecked = checked
+            refreshDrawableState()
+            onCheckListener?.onCheckedChanged(this, checked)
+        }
+    }
+
+    fun toggle() {
+        setChecked(!isChecked)
+    }
+
+    fun isChecked(): Boolean {
+        return isChecked
+    }
+
+    fun setCheckListener(onCheckListener: OnCheckListener?) {
+        this.onCheckListener = onCheckListener
+    }
+
+    fun interface OnCheckListener {
+        fun onCheckedChanged(imageCheckView: ImageCheckView, isChecked: Boolean)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        onCheckListener = null
+    }
+
+}
