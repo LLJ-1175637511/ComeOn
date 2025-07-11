fb757c7341d84af97ddae3d17de6ed29664d7d00
liulinjie <liulinjie@zhihu.com>
2025-07-11 10:44:49 +0800

    viewUtils

--- library/src/main/java/com/comeon/android/library/ui/fresco/ext/setDynamicImageUrl.kt
+++ library/src/main/java/com/comeon/android/library/ui/fresco/ext/setDynamicImageUrl.kt
@@ -2,14 +2,34 @@ package com.comeon.android.library.ui.fresco.ext
 
 import android.net.Uri
 import com.comeon.android.library.ui.fresco.image.WrapContentControllerListener
+import com.facebook.common.util.UriUtil
 import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder
+import com.facebook.drawee.controller.AbstractDraweeController
+import com.facebook.drawee.interfaces.DraweeController
 import com.facebook.drawee.view.SimpleDraweeView
+import java.net.URI
+
+fun SimpleDraweeView.setDynamicImageUrl(uri: Uri, isAutoPlay: Boolean = true) {
+    controller = configDynamicController(uri) {
+        controllerListener = WrapContentControllerListener(this@setDynamicImageUrl)
+        autoPlayAnimations = isAutoPlay
+    }
+}
 
 fun SimpleDraweeView.setDynamicImageUrl(url: String, isAutoPlay: Boolean = true) {
-    controller = Fresco.newDraweeControllerBuilder()
-        .setOldController(controller)
-        .setControllerListener(WrapContentControllerListener(this))
-        .setUri(Uri.parse(url))
-        .setAutoPlayAnimations(isAutoPlay)
-        .build()
+    setDynamicImageUrl(Uri.parse(url), isAutoPlay)
+}
+
+fun SimpleDraweeView.setDynamicImageUrl(res: Int, isAutoPlay: Boolean = true) {
+    setDynamicImageUrl(UriUtil.getUriForResourceId(res), isAutoPlay)
+}
+
+private fun SimpleDraweeView.configDynamicController(uri: Uri, block: PipelineDraweeControllerBuilder.() -> Unit): DraweeController {
+    return Fresco.newDraweeControllerBuilder().apply {
+        oldController = controller
+        autoPlayAnimations = true
+        setUri(uri)
+        block()
+    }.build()
 }
